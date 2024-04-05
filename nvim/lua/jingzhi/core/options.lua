25d487a4eda3573cd72996beb7355f8e935440fa
Jingzhi Li <jingzhi.li@uni-wuerzburg.de>
2024-04-05 18:24:07 +0200

    Remove ignore case searching

--- nvim/lua/jingzhi/core/options.lua
+++ nvim/lua/jingzhi/core/options.lua
@@ -14,8 +14,8 @@ opt.autoindent = true -- copy indent from current line when starting new one
 -- opt.wrap = false -- disable line wrapping
 
 -- search settings
-opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+-- opt.ignorecase = true -- ignore case when searching
+-- opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
